c7bc5742766abcb4f688d6f086a7c7e2ae7de0c7
Christa Stephens <ChristaStephens@outlook.com>
2018-12-18 18:46:45 -0500

    changed the methods for the app

--- src/CalendarApp/App.java
+++ src/CalendarApp/App.java
@@ -4,40 +4,32 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-
 public class App {
 
 	public static void main(String[] args) {
-//		Goal is to create a motivational quote app
-//		with each day/quote refreshing and potentially
-//		giving a new background & quote.
+		// Goal is to create a motivational quote app
+		// with each day/quote refreshing and potentially
+		// giving a new background & quote.
 
+		// String day = new SimpleDateFormat("EEEE").format(new Date());
+		System.out.println(thisIsToday());
 
-//		Scanner scnr = new Scanner (System.in);
-//		String date;
-//		System.out.println("Please enter a date");
-//		date = scnr.nextLine();
-//		
-		// instead of having it in private it, use it in a regular string
-		//swich around the formatof timestamp to read ddmmyyhhmmss
-		//can create in their own methods to return at top to say (day and time)
-		
-		
+		// String timeStamp = new SimpleDateFormat("MM/dd/YYYY hh:mm
+		// a").format(Calendar.getInstance().getTime());
+		System.out.println(dayWithTime());
+
+	}
+
+	protected static String thisIsToday() {
 		String day = new SimpleDateFormat("EEEE").format(new Date());
-		System.out.println(day);
-	
+
+		return day;
+
+	}
+
+	protected static String dayWithTime() {
 		String timeStamp = new SimpleDateFormat("MM/dd/YYYY hh:mm a").format(Calendar.getInstance().getTime());
-		System.out.println(timeStamp);
-		
-		
+		return timeStamp;
 	}
-	protected String thisistoday() {
-		// TODO Auto-generated method stub
-		String day = new SimpleDateFormat("EEEE").format(new Date());
-		System.out.println(day);
-		return  day;
-		
-	}
-	
 
 }
